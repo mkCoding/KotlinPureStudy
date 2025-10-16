2133de5b24b741341f9241fd7a3d95f34fbfc441
mikekemakolam <mkem50@gmail.com>
2025-10-16 15:47:53 -0500

    committing Inheritance Study

---
+++ app/src/main/java/com/example/kotlinpurestudy/InheritanceStudyApp.kt
@@ -0,0 +1,35 @@
+package com.example.kotlinpurestudy
+
+fun main(){
+
+    val dog = Dog()
+    dog.eat() // inherited from Animal Class
+    dog.sleep() // inherited from Animal Class
+    dog.move() // inherited from Animal Class
+    dog.bark()
+    dog.howl()
+    dog.wagTail()
+}
+
+
+open class Animal{
+    fun eat(){}
+    fun sleep(){}
+    fun move(){}
+}
+
+class Dog:Animal() {
+    // Dog inherits from Animal
+    fun bark(){
+        println("Dog is barking")
+    }
+
+    fun wagTail(){
+        println("Dog tail is wagging")
+    }
+
+    fun howl(){
+        println("Dog is howling")
+    }
+
+}
