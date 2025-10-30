612edd324c40582dfc64d234301c4df566ec6378
mikekemakolam <mkem50@gmail.com>
2025-10-30 11:08:30 -0500

    saving lambada study

---
+++ app/src/main/java/com/example/kotlinpurestudy/lambada_study/LambadaStudyApp.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinpurestudy.lambada_study
+
+fun main() {
+
+// lambada - anonymous function
+
+    // 2 params
+    val sum = { a: Int, b: Int -> a * b }
+    val result = sum(20, 2)
+    println(result)
+
+    // 1 param
+    val square: (Int) -> Int = { it * it }
+    println(square(3))
+
+    // multiply every element in list by 2
+    val myList = listOf(1, 2, 3, 4, 5, 6, 7)
+    val double = myList.map {it * 2}
+
+    println(double)
+}
+
