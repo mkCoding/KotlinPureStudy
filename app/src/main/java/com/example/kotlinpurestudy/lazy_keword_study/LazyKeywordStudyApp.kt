422a0814d16267e8a782da5b5b7571b7a59022ed
mikekemakolam <mkem50@gmail.com>
2025-12-16 04:07:28 -0500

    updating lazy keyword study

--- app/src/main/java/com/example/kotlinpurestudy/lazy_keword_study/LazyKeywordStudyApp.kt
+++ app/src/main/java/com/example/kotlinpurestudy/lazy_keword_study/LazyKeywordStudyApp.kt
@@ -2,21 +2,25 @@ package com.example.kotlinpurestudy.lazy_keword_study
 
 fun main(){
 
-    val executeBark by lazy {
-        {
-            val dog = Dog()
-            dog.bark()
-        }
-    }
+    // Lazy - calculate a value on first access
 
-    val shouldDogBark = true
+    /*
+    this will ensure dog object instance is not created until its actually needed
+     */
+    val dog by lazy {Dog()}
 
-    if(shouldDogBark){
-        executeBark()
+    val shouldBark = true // could be true or false
+
+    if (shouldBark){
+        dog.bark()
     }else{
         // do nothing
     }
 
+    /*
+    summary if we didn't use lazy above the Dog instance will be created whether
+    should bark is true or not
+     */
 }
 
 class Dog {
